904514a2eb5aa0ca9c8b2e310f54bf494b0e13a3
HongRu <case13486@gmail.com>
2020-02-04 14:28:45 +0800

    update err

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Account } from './model/account.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +8,7 @@ import { Account } from './model/account.model';
 export class AppComponent {
   title = 'demo01';
   age = 10;
-  acc: Account = {
-    account : '123',
-    password : 'abc'
-  };
+
   weeks = ['星期一', '星期二', '星期三'];
   now = new Date();
   sayHi() {
